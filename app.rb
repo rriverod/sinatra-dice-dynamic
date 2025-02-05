37f9b234326b5069d7367263679580058fa0c4fc
rriverod <rriverod@chicagobooth.edu>
2025-02-05 23:13:17 +0000

    Dynamic die

--- app.rb
+++ app.rb
@@ -5,50 +5,17 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
+get("/dice/:number_of_die/:size_of_die") do
+  @num_dice=params.fetch("number_of_die").to_i
+  @size_dice=params.fetch("size_of_die").to_i
+
   @rolls = []
 
-  2.times do
-    die = rand(1..6)
+@num_dice.times do
+    die = rand(1..@size_dice)
 
     @rolls.push(die)
   end
 
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
+  erb(:flexible)
 end
